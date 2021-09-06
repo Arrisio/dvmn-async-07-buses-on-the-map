46ad4dde2b78ce98d8529bcb6d37c7f625cfd678
Dmitriy Suchkov <Account.log@yandex.ru>
2021-09-06 15:46:32 +0300

    feat: 6/21 read routes from files

--- fake_bus.py
+++ fake_bus.py
@@ -1,22 +1,32 @@
 import json
+import os
 
 import trio
 from sys import stderr
 from trio_websocket import open_websocket_url
 
+def load_routes(directory_path='routes'):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r', encoding='utf8') as file:
+                yield json.load(file)
+
+
 
 async def main():
     try:
         async with open_websocket_url("ws://127.0.0.1:8080") as ws:
-            while True:
-                bus_data = {
-                    "busId": "c790сс",
-                    "lat": 55.747629944737,
-                    "lng": 37.641726387317,
-                    "route": "156",
-                }
+            for rout_data in load_routes():
+                for lat, lng in rout_data['coordinates']:
+                    bus_data = {
+                        "busId": rout_data['name'],
+                        "lat": lat,
+                        "lng": lng,
+                        "route": rout_data['name'],
+                    }
                 await ws.send_message(json.dumps(bus_data))
-                await trio.sleep(.1)
+                # await trio.sleep(.1)
             # await ws.get_message()
     except OSError as ose:
         print("Connection attempt failed: %s" % ose, file=stderr)
